e3ea17ed5c3687cc173e23f291f657593d88a7ee
Justin Russo <j.russo@outlook.com>
2022-01-07 19:16:38 -0500

    feat(items): add items GET route

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
 from .models import db, Item, User
-from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.item_routes import item_routes
+from .api.user_routes import user_routes
 
 from .seeds import seed_commands
 
@@ -29,8 +30,9 @@ def load_user(id):
 app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
-app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(item_routes, url_prefix='/api/items')
+app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
 Migrate(app, db)
 
