db24e1698a42d40292fe40247ba2ec52cad8dfdf
isaacsungpak <sungyulpak@gmail.com>
2022-01-07 20:01:31 -0500

    home first few lines

---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import NavBar from "../NavBar";
+
+function HomePage() {
+    const dispatch = useDispatch()
+    const user = useSelector((state) => state.session.user)
+    const items = useSelector((state) => state.items?.entities?.items) // remove optional chaining after store is set up
+
+    return (
+        <>
+            <NavBar/>
+            <p>{user != undefined ? `Welcome back, ${user.username}` : 'Find rare game items (temp. message)'}</p>
+        </>
+    )
+}
