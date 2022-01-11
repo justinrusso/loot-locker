9508d568902fcc1767f13ac4b27df114fd74fea2
isaacsungpak <sungyulpak@gmail.com>
2022-01-11 16:20:09 -0500

    home page catgory circles and etc.

--- react-app/src/components/HomePage/Category.js
+++ react-app/src/components/HomePage/Category.js
@@ -1,52 +1,71 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 
-function Category({ path, name, source }) {
-    const CategoryBlock = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 3vw;
-    width: 7vw;
+const CategoryBlock = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 3vw;
+        width: 7vw;
 
-    &:hover {
-        border-bottom: 2px solid black;
-    }
+        .category-name {
+            text-align: center;
+            width: 100%;
+            padding-bottom: 5px;
+            border-bottom: 2px solid transparent;
+        }
 
-    &:hover .circle {
-        transform: scale(1.08);
-        filter: drop-shadow(0 1vh 1vh rgba(0,0,0,0.3))
-    }
+        &:hover .category-name {
+            border-bottom: 2px solid black;
+        }
 
-    &:hover a {
-        color: #595959
-    }
+        &:hover .circle {
+            transform: scale(1.08);
+            filter: drop-shadow(0 1vh 1vh rgba(0,0,0,0.3))
+        }
 
-    a {
-        color: black;
-        text-decoration: none;
-        font-size: 16px;
-        font-weight: 700;
-        line-height: 20px;
-    }
+        &:hover a {
+            color: #595959
+        }
 
-    .circle {
-        width: 114px;
-        height: 114px;
-        border: 2px black solid;
-        border-radius: 50%;
-        margin-bottom: 2vh;
-        transition: all 0.2s ease-in-out;
-    }
+        a {
+            color: black;
+            text-decoration: none;
+            font-size: 16px;
+            font-weight: 700;
+            line-height: 20px;
+        }
+
+        .img-wrapper {
+            position: relative;
+            padding-top: 100%;
+            width: 114px;
+            margin: 0 12px;
+        }
+
+        .circle {
+            width: 114px;
+            height: 114px;
+            border: 2px black solid;
+            border-radius: 50%;
+            transition: all 0.2s ease-in-out;
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
     `
+
+function Category({ path, name, source }) {
 
     return (
         <CategoryBlock>
             <Link href={path}>
-                <img src={source} className="circle"/>
+                <div className="img-wrapper">
+                    <img src={source} className="circle"/>
+                </div>
             </Link>
-            <Link href={path}>{name}</Link>
+            <Link href={path} className="category-name">{name}</Link>
         </CategoryBlock>
     )
 }
