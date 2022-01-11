29a7412a2952dfdc52bcccfd150c164f58eaec80
Justin Russo <j.russo@outlook.com>
2022-01-11 11:44:30 -0500

    feat(cart): add item to cart backend route

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,23 +1,12 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import User, db
-from app.forms import LoginForm
+from app.forms import LoginForm, validation_errors_to_error_messages
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
 from sqlalchemy import or_
 
+
 auth_routes = Blueprint("auth", __name__)
-
-
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f"{error}")
-    return errorMessages
-
 
 @auth_routes.route("/")
 def authenticate():
--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import current_user, login_required
+from app.forms import CartItemForm, validation_errors_to_error_messages
 
-from app.models import CartItem, Item
+from app.models import CartItem, Item, db
 
 
 cart_routes = Blueprint('cart', __name__)
@@ -11,3 +12,19 @@ cart_routes = Blueprint('cart', __name__)
 def cart_items():
     cart_items = CartItem.query.filter(CartItem.user_id == current_user.id).join(Item).all()
     return {'cartItems': [cart_item.to_dict() for cart_item in cart_items]}
+
+@cart_routes.route('/', methods=['POST'])
+@login_required
+def add_cart_item():
+    form = CartItemForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        cart_item = CartItem(
+            user_id=current_user.id,
+            item_id=form.data['itemId'],
+            quantity=form.data['quantity']
+        )
+        db.session.add(cart_item)
+        db.session.commit()
+        return cart_item.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 400
