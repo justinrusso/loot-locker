7bd5bf30a7d38216eb893e711a107e80c42891c1
isaacsungpak <sungyulpak@gmail.com>
2022-01-11 14:49:03 -0500

    reverted categories block from ul to div

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -13,8 +13,6 @@ const HomeStyling = styled.div`
 
     #home-top {
         postion: absolute;
-        display: block;
-        z-index: 2;
     }
 
     h1 {
@@ -36,8 +34,6 @@ const HomeStyling = styled.div`
         display: flex;
         justify-content: center;
         align-items: flex-start;
-        list-style-type: none;
-        padding: 0px;
     }
 
     .section-title {
@@ -89,13 +85,13 @@ function HomePage() {
             <div id="home-top">
                 <h1 id="header-1">{user != undefined ? `Welcome back, ${user.username}` : 'Find rare game items (temp. message)'}</h1>
 
-                <ul id="category-container">
-                    <li><Category path="/tags/1" name="Arms" source={false} /></li>
-                    <li><Category path="/tags/2" name="Armor" source={false} /></li>
-                    <li><Category path="/tags/3" name="Accessories" source={false} /></li>
-                    <li><Category path="/tags/4" name="Mounts" source={false} /></li>
-                    <li><Category path="/tags/5" name="Consumables" source={false}/></li>
-                </ul>
+                <div id="category-container">
+                    <Category path="/tags/1" name="Arms" source={false} />
+                    <Category path="/tags/2" name="Armor" source={false} />
+                    <Category path="/tags/3" name="Accessories" source={false} />
+                    <Category path="/tags/4" name="Mounts" source={false} />
+                    <Category path="/tags/5" name="Consumables" source={false}/>
+                </div>
             </div>
 
 
