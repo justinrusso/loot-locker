a3d78908f9deb84eb73eda3b56e8189fe1fa2133
Justin Russo <j.russo@outlook.com>
2022-01-11 12:27:39 -0500

    feat: add a css reset

--- react-app/src/theme/GlobalStyle.js
+++ react-app/src/theme/GlobalStyle.js
@@ -1,8 +1,67 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
+
+/*
+  Josh's Custom CSS Reset
+  https://www.joshwcomeau.com/css/custom-css-reset/
+*/
+const resetCss = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  // https://www.joshwcomeau.com/css/custom-css-reset/#digit-tweaking-line-height
+  * {
+    line-height: calc(1em + 0.5rem);
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+`;
 
 const GlobalStyle = createGlobalStyle`
+    ${resetCss}
+
     body {
-        margin: 0;
         font-family: 'Roboto', sans-serif;
     }
 
