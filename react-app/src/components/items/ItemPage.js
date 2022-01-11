616ef704b4bc9b2103d7aec5a41ccbdd14e224a2
andyrose507 <drew.jones1998@gmail.com>
2022-01-11 12:02:41 -0700

    item page now successfully gets its displayed content from store

--- react-app/src/components/items/ItemPage.js
+++ react-app/src/components/items/ItemPage.js
@@ -41,6 +41,7 @@ const StyledItemPageDiv = styled.div`
                   padding-right: 5%;
             }
 
+
             button {
                   margin-left: 5%;
                   margin-right: 5%;
@@ -116,18 +117,6 @@ const ItemPage = () => {
 
       const [showDescription, setShowDescription] = useState(true)
 
-      const testItem = {
-            user_id: 1,
-            name: "Heart Container | +1 Heart",
-            description:
-            `
-            Heart Containers, also known as Container Hearts, are recurring items in the Legend of Zelda series. These heart-shaped containers are visual representations of Link's current health. The more Heart Containers Link collects, the more health he will have. The number of Heart Containers Link has reflects the maximum amount of health he can have at one time, traditionally displayed in the upper left corner of the screen. When Link's health is full, the Heart Containers are red. As Link loses health, they lose their color or disappear. In some games, Link can lose quarter hearts or half hearts.
-            Link usually starts his adventure with three Heart Containers; the limit for how many Heart Containers Link can obtain in all depends on the game, though the most common maximum is twenty. Heart Containers are typically left behind by bosses upon defeat. Link can also "create" Heart Containers by collecting Pieces of Heart; the most common number of Pieces of Heart needed to complete another Heart Container is four, however, exceptions exist. Some games omit Pieces of Heart in favor of completed Heart Containers. Occasionally, Link will need a specific number of Heart Containers to complete certain tasks.
-            `,
-            image: "https://purenintendo.com/wp-content/uploads/2012/03/LOZ_OoT_3D_HeartPiece.png",
-            price: 500,
-            stock: 5
-      }
 
       const handleSetShowDescription = () => {
             setShowDescription(!showDescription)
@@ -145,20 +134,20 @@ const ItemPage = () => {
                         <div id="item-name">{testItem.name}</div>
                         <div id="item-price">
                               <i className="fas fa-coins" id="coins-icon"></i>
-                              {testItem.price}
-                              {testItem.stock > 0 && <span><i className="fas fa-check"></i> In stock</span>}
-                              {testItem.stock === 0 && <span><i className="fas fa-times"></i> Out of stock</span>}
+                              {item.price}
+                              {item.stock > 0 && <span><i className="fas fa-check"></i> In stock</span>}
+                              {item.stock === 0 && <span><i className="fas fa-times"></i> Out of stock</span>}
                         </div>
                         <button id="add-to-cart-button">
                               <span>Add to cart </span>
-                              {testItem.stock < 6 && <>| Only {testItem.stock} available</>}
+                              {item.stock < 6 && <>| Only {item.stock} available</>}
                         </button>
                         <button onClick={handleSetShowDescription}id="description-button">
                               <span>Description</span>
                               {!showDescription && <i className="fas fa-chevron-down"></i>}
                               {showDescription && <i className="fas fa-chevron-up"></i>}
                         </button>
-                        {showDescription && <div id="item-description">{testItem.description}</div>}
+                        {showDescription && <div id="item-description">{item.description}</div>}
                   </div>
             </StyledItemPageDiv>
       )
