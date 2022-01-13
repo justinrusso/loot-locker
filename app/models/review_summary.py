ef79b04086225db9f3c55a870e64dbe63216dff9
John J. Lee <johnlee1120@gmail.com>
2022-01-12 16:03:52 -0800

    ratings table model

---
+++ app/models/review_summary.py
@@ -0,0 +1,15 @@
+from .db import db
+
+
+class ReviewSummary(db.Model):
+    __tablename__ = 'review_summaries'
+
+    item_id = db.Column(db.Integer, db.ForeignKey(
+        "items.id"), primary_key=True, )
+    num_of_reviews = db.Column(db.Integer, nullable=False)
+    ratings_total = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'rating': self.ratings_total / self.num_of_reviews
+        }
