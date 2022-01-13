773f79a7647e2f073a29ca89782bb6da910d569c
John J. Lee <johnlee1120@gmail.com>
2022-01-13 13:29:42 -0800

    Remove unnecessary imports in review routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, abort
-from flask_login import current_user, login_required
+from flask import Blueprint, request
+from flask_login import login_required
 from app.forms import ReviewForm, DeleteReviewForm, validation_errors_to_error_messages
 
 from app.models import db, Review, ReviewSummary
