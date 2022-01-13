4bbe9306eb4293b2d1e8db1dcaee5781c11a905a
Justin Russo <j.russo@outlook.com>
2022-01-13 15:49:15 -0500

    feat: style Signup form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,23 +1,56 @@
-import styled from 'styled-components';
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import styled from "styled-components";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const Actions = styled.div`
+import Button from "../common/Button";
+import InputField from "../common/InputField";
+import { signUp } from "../../store/session";
+
+const Heading = styled.div`
   display: flex;
   justify-content: space-between;
-  padding-top: 16px;
+  width: 100%;
+  padding-bottom: 24px;
+
+  button {
+    font-size: 13px;
+    line-height: 1.4;
+    padding: 9px 15px;
+  }
+`;
+
+const InputsWrapper = styled.div`
+  gap: 8px;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  margin-bottom: 16px;
+`;
+
+const PasswordInputsWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 8px;
+
+  @media (min-width: 600px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+const RegisterButton = styled(Button)`
+  width: 100%;
+  margin-bottom: 16px;
 `;
 
 const SignUpForm = ({ onSuccess, toLogin }) => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [location, setLocation] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [location, setLocation] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
@@ -34,93 +67,87 @@ const SignUpForm = ({ onSuccess, toLogin }) => {
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updateLocation = (e) => {
-    setLocation(e.target.value);
-  };
-
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>Username</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-          required
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-          required
-        ></input>
-      </div>
-      <div>
-        <label>Location</label>
-        <input
-          type='text'
-          name='location'
-          onChange={updateLocation}
-          value={location}
-          required
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-          required
-        ></input>
-      </div>
-      <div>
-        <label>Confirm Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required
-        ></input>
-      </div>
-      <Actions>
-        <button type='button' onClick={toLogin}>Log In</button>
-        <button type='submit'>Register</button>
-      </Actions>
-    </form>
+    <>
+      <Heading>
+        <h2>Register</h2>
+        <Button type="button" variant="outlined" onClick={toLogin}>
+          Log In
+        </Button>
+      </Heading>
+      <form onSubmit={onSignUp}>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <InputsWrapper>
+          <InputField
+            fullWidth
+            label="Username"
+            value={username}
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+            inputProps={{
+              autoFocus: true,
+              type: "text",
+            }}
+            required
+          />
+          <InputField
+            fullWidth
+            label="Email"
+            value={email}
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            inputProps={{
+              type: "text",
+            }}
+            required
+          />
+          <InputField
+            fullWidth
+            label="Location"
+            value={location}
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            inputProps={{
+              type: "text",
+            }}
+            required
+          />
+          <PasswordInputsWrapper>
+            <InputField
+              fullWidth
+              label="Password"
+              value={password}
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              inputProps={{
+                type: "password",
+              }}
+              required
+            />
+            <InputField
+              fullWidth
+              label="Confirm Password"
+              value={repeatPassword}
+              id="repeat-password"
+              onChange={(e) => setRepeatPassword(e.target.value)}
+              inputProps={{
+                type: "password",
+              }}
+              required
+            />
+          </PasswordInputsWrapper>
+        </InputsWrapper>
+        <RegisterButton type="submit">Register</RegisterButton>
+      </form>
+    </>
   );
 };
 
