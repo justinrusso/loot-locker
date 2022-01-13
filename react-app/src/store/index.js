0d42aa3ab7d8278822ab9adb3e480b0cb748ef6d
isaacsungpak <sungyulpak@gmail.com>
2022-01-12 19:34:58 -0500

    added categories to state and added thunk for get

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,6 +1,7 @@
 import cartItems from "./cart-items";
 import session from "./session";
 import items from "./items";
+import categories from "./categories";
 import { configureStore } from "@reduxjs/toolkit";
 
 const isDev = process.env.NODE_ENV !== "production";
@@ -9,7 +10,8 @@ const store = configureStore({
   reducer: {
     cartItems,
     session,
-    items
+    items,
+    categories
   },
   middleware: (getDefaultMiddleware) => {
     if (isDev) {
